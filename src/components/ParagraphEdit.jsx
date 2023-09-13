e446a9d3ab3a2a4a7c359f4ddd5e339df2323527
Kyle J. Harms <kyle.harms@cornell.edu>
2023-09-13 16:11:33 -0400

    events

--- src/components/ParagraphEdit.jsx
+++ src/components/ParagraphEdit.jsx
@@ -4,19 +4,27 @@ import './ParagraphEdit.css';
 
 export default function ParagraphEdit({ children, editMode = false }) {
 
+  function handleSaveText() {
+    console.log("TODO: save text");
+  }
+
+  function handleEditText() {
+    console.log("TODO: edit text");
+  }
+
   let componentContent;
   if (editMode) {
     componentContent = (
       <>
         <textarea defaultValue={children} rows={5} />
-        <button>Save</button>
+        <button onClick={handleSaveText}>Save</button>
       </>
     )
   } else {
     componentContent = (
       <>
         <p>{children}</p>
-        <button>Edit</button>
+        <button onClick={handleEditText}>Edit</button>
       </>
     )
   }
