ea2b8f269f2f6f363ac7dd1a599fe7b632ead6a9
Kyle J. Harms <kyle.harms@cornell.edu>
2023-09-13 16:13:13 -0400

    save state

--- src/components/ParagraphEdit.jsx
+++ src/components/ParagraphEdit.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './ParagraphEdit.css';
 
 export default function ParagraphEdit({ children, edit = false }) {
+  const [content, setContent] = useState(children);
   const [editMode, setEditMode] = useState(edit);
 
   function handleSaveText() {
@@ -17,7 +18,10 @@ export default function ParagraphEdit({ children, edit = false }) {
   if (editMode) {
     componentContent = (
       <>
-        <textarea defaultValue={children} rows={5} />
+        <textarea
+          value={content}
+          onChange={e => setContent(e.target.value)}
+          rows={5} />
         <button onClick={handleSaveText}>Save</button>
         <button onClick={() => setEditMode(false)}>Save (arrow function)</button>
       </>
@@ -25,7 +29,7 @@ export default function ParagraphEdit({ children, edit = false }) {
   } else {
     componentContent = (
       <>
-        <p>{children}</p>
+        <p>{content}</p>
         <button onClick={handleEditText}>Edit</button>
         <button onClick={() => setEditMode(true)}>Edit (arrow function)</button>
       </>
